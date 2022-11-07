c8304c9d12c558a86db8cb2c396af07a7a1d8ef9
Charlie Smith <charlie.smith@opencastsoftware.com>
2022-11-07 13:40:45 +0000

    add -r flag to increment script

---
+++ increment_version.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [[ "$(git rev-parse --abbrev-ref HEAD)" == "main" ]]; then
+	read -rp "Enter commit message: " commit_message &&
+		auto-version --minor &&
+		git add . && git commit -am "$commit_message" &&
+		git push &&
+		git tag "$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')"
+fi
