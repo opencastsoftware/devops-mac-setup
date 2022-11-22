fd51c9e3e9e465e6ca35e8bddb7a5b797cf64951
Charlie Smith <charlie.smith@opencastsoftware.com>
2022-11-22 15:14:53 +0000

    Add Mkdocs config

--- increment_version.sh
+++ increment_version.sh
@@ -4,5 +4,5 @@ if [[ "$(git rev-parse --abbrev-ref HEAD)" == "main" ]]; then
 		auto-version --minor &&
 		git add . && git commit -am "$commit_message" &&
 		git tag "$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')"
-		git push
+	git push
 fi
